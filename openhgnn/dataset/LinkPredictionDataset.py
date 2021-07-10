dbc0cc5bbcea6ce793504389700e932c6f596adc
Theheavens <34649403+Theheavens@users.noreply.github.com>
2021-07-10 21:49:42 +0800

    Update LinkPredictionDataset.py

--- openhgnn/dataset/LinkPredictionDataset.py
+++ openhgnn/dataset/LinkPredictionDataset.py
@@ -21,6 +21,50 @@ class LinkPredictionDataset(BaseDataset):
         super(LinkPredictionDataset, self).__init__()
 
 
+@register_dataset('test_link_prediction')
+class Test_LinkPrediction(LinkPredictionDataset):
+    def __init__(self, dataset_name):
+        super(Test_LinkPrediction, self).__init__()
+        self.g = self.load_HIN('./openhgnn/debug/data.bin')
+        self.target_link = 'user-item'
+        self.has_feature = False
+        self.preprocess()
+        #self.generate_negative()
+
+    def load_HIN(self, dataset_name):
+        g, _ = load_graphs(dataset_name)
+        return g[0]
+
+    def preprocess(self):
+        test_mask = self.g.edges[self.target_link].data['test_mask']
+        index = th.nonzero(test_mask).squeeze()
+        self.test_edge = self.g.find_edges(index, self.target_link)
+        self.pos_test_graph = dgl.heterograph({('user', 'user-item', 'item'): self.test_edge}, {ntype: self.g.number_of_nodes(ntype) for ntype in ['user', 'item']})
+        self.g.remove_edges(index, self.target_link)
+        self.g.remove_edges(index, 'item-user')
+        self.neg_test_graph, _ = dgl.load_graphs('./openhgnn/debug/neg.bin')
+        self.neg_test_graph = self.neg_test_graph[0]
+        return
+
+    def generate_negative(self):
+        k = 99
+        e = self.pos_test_graph.edges()
+        neg_src = []
+        neg_dst = []
+        for i in range(self.pos_test_graph.number_of_edges()):
+            src = e[0][i]
+            exp = self.pos_test_graph.successors(src)
+            dst = th.randint(high=self.g.number_of_nodes('item'), size=(k,))
+            for d in range(len(dst)):
+                while dst[d] in exp:
+                    dst[d] = th.randint(high=self.g.number_of_nodes('item'), size=(1,))
+            src = src.repeat_interleave(k)
+            neg_src.append(src)
+            neg_dst.append(dst)
+        neg_edge = (th.cat(neg_src), th.cat(neg_dst))
+        neg_graph = dgl.heterograph({('user', 'user-item', 'item'): neg_edge}, {ntype: self.g.number_of_nodes(ntype) for ntype in ['user', 'item']})
+        dgl.save_graphs('./openhgnn/debug/neg.bin', neg_graph)
+
 @register_dataset('hin_link_prediction')
 class HIN_LinkPrediction(LinkPredictionDataset):
     def __init__(self, dataset_name):
@@ -49,7 +93,6 @@ class HIN_LinkPrediction(LinkPredictionDataset):
             self.test_batch = self.load_link_pred('./openhgnn/dataset/' + dataset_name + '/a_a_list_test.txt')
             self.category = 'author'
         return g
-
 
 
 
