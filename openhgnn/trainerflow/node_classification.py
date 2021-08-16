a62c1891ebcfef007b4f0e560d19397beaab401a
la <502736806@qq.com>
2021-08-16 23:57:43 +0800

    deal with float problem

--- openhgnn/trainerflow/node_classification.py
+++ openhgnn/trainerflow/node_classification.py
@@ -30,7 +30,7 @@ class NodeClassification(BaseFlow):
         self.model_name = args.model
         self.device = args.device
         self.task = build_task(args)
-        self.metric = args.metric
+        self.metric = args.metric if hasattr(args, 'metric') else None
 
         self.hg = self.task.get_graph().to(self.device)
         self.num_classes = self.task.dataset.num_classes
