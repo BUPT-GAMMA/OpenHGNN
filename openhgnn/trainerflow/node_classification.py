84d7e0b9cd3f2ca7a3d30a010634c42f64636502
Theheavens <34649403+Theheavens@users.noreply.github.com>
2021-08-16 22:12:38 +0800

    Update node_classification.py

--- openhgnn/trainerflow/node_classification.py
+++ openhgnn/trainerflow/node_classification.py
@@ -17,9 +17,7 @@ class NodeClassification(BaseFlow):
     """Node classification flows.
     Supported Model: HAN/MAGNN/GTN
     Supported Dataset：ACM
-
     The task is to classify the nodes of HIN(Heterogeneous Information Network).
-
     Note: If the output dim is not equal the number of classes, a MLP will follow the gnn model.
     """
 
@@ -30,6 +28,7 @@ class NodeClassification(BaseFlow):
         self.model_name = args.model
         self.device = args.device
         self.task = build_task(args)
+        self.metric = args.metric
 
         self.hg = self.task.get_graph().to(self.device)
         self.num_classes = self.task.dataset.num_classes
@@ -71,6 +70,18 @@ class NodeClassification(BaseFlow):
             else:
                 # self.model = MLP_follow_model(self.model, args.out_dim, self.num_classes)
                 pass
+        elif self.args.model == 'MHNF':
+            if hasattr(self.args, 'adaptive_lr_flag') and self.args.adaptive_lr_flag == True:
+                self.optimizer = torch.optim.Adam([{'params': self.model.gcn_list.parameters()},
+                                                   {'params': self.model.channel_attention.parameters()},
+                                                   {'params': self.model.layers_attention.parameters()},
+                                                   {'params': self.model.linear.parameters()},
+                                                   {"params": self.model.HLHIA_layer.layers.parameters(), "lr": 0.5}
+                                                   ], lr=0.005, weight_decay=0.001)
+
+            else:
+                # self.model = MLP_follow_model(self.model, args.out_dim, self.num_classes)
+                pass
         return
 
     def train(self):
@@ -83,30 +94,56 @@ class NodeClassification(BaseFlow):
             else:
                 loss = self._full_train_step()
             #if (epoch + 1) % self.evaluate_interval == 0:
-            f1, losses = self._test_step()
+            score, losses = self._test_step()
 
-            train_f1 = f1["train"]
-            val_f1 = f1["val"]
-            test_f1 = f1['test']
+            train_score = score["train"]
+            val_score = score["val"]
+            test_score = score['test']
             val_loss = losses["val"]
             # epoch_iter.set_description(
             #     f"Epoch: {epoch:03d}, Train_macro_f1: {train_f1[0]:.4f}, Train_micro_f1: {train_f1[1]:.4f}, Val_macro_f1: {val_f1[0]:.4f}, Val_micro_f1: {val_f1[1]:.4f}, ValLoss:{val_loss: .4f}"
             # )
-            print((
-                f"Epoch: {epoch:03d}, Loss: {loss:.4f}, Train_macro_f1: {train_f1[0]:.4f}, Train_micro_f1: {train_f1[1]:.4f}, "
-                f"Val_macro_f1: {val_f1[0]:.4f}, Test_macro_f1: {test_f1[0]:.4f}, ValLoss:{val_loss: .4f}"
-            ))
-            early_stop = stopper.step(val_loss, val_f1[0], self.model)
+            self.printInfo(epoch, loss, train_score, val_score, test_score, val_loss)
+
+            early_stop = stopper.step(val_loss, val_score, self.model)
             if early_stop:
                 print('Early Stop!\tEpoch:' + str(epoch))
                 break
 
-        print(f"Valid_micro_f1 = {stopper.best_score: .4f}, Min_loss = {stopper.best_loss: .4f}")
+        print(f"Valid_micro_{self.metric} = {stopper.best_score: .4f}, Min_loss = {stopper.best_loss: .4f}")
         stopper.load_model(self.model)
-        test_f1, _ = self._test_step(split="test")
-        val_f1, _ = self._test_step(split="val")
-        print(f"Test_macro_f1 = {test_f1[0]:.4f}, Test_micro_f1: {test_f1[1]:.4f}")
-        return dict(Acc=test_f1, ValAcc=val_f1)
+        test_score, _ = self._test_step(split="test")
+        val_score, _ = self._test_step(split="val")
+        if isinstance(test_score, tuple):
+            print(f"Test_macro_{self.metric} = {test_score[0]:.4f}, Test_micro_{self.metric}: {test_score[1]:.4f}")
+        else:
+            print(f"Test_{self.metric} = {test_score:.4f}")
+        return dict(Acc=test_score, ValAcc=val_score)
+
+    def printInfo(self, epoch, loss, train_score, val_score, test_score, val_loss):
+
+        if self.metric == 'f1_lr':
+            print((
+                f"Epoch: {epoch:03d}, Loss: {loss:.4f}, Train_macro_f1: {train_score[0]:.4f}, Train_micro_f1: {train_score[1]:.4f}, "
+                f"Val_macro_f1: {val_score[0]:.4f}, Test_macro_f1: {test_score[0]:.4f}, ValLoss:{val_loss: .4f}"
+            ))
+        # use acc
+        elif self.metric == 'acc':
+            print((
+                f"Epoch: {epoch:03d}, Loss: {loss:.4f}, Train_acc: {train_score:.4f},  "
+                f"Val_acc: {val_score:.4f}, Test_acc: {test_score:.4f}, ValLoss:{val_loss: .4f}"
+            ))
+        elif self.metric == 'acc-ogbn-mag':
+            print((
+                f"Epoch: {epoch:03d}, Loss: {loss:.4f}, Train_acc: {train_score:.4f},  "
+                f"Val_acc: {val_score:.4f}, Test_acc: {test_score:.4f}, ValLoss:{val_loss: .4f}"
+            ))
+        else :
+            print((
+                f"Epoch: {epoch:03d}, Loss: {loss:.4f}, Train_macro_f1: {train_score[0]:.4f}, Train_micro_f1: {train_score[1]:.4f}, "
+                f"Val_macro_f1: {val_score[0]:.4f}, Test_macro_f1: {test_score[0]:.4f}, ValLoss:{val_loss: .4f}"
+            ))
+
 
     def _full_train_step(self):
         self.model.train()
@@ -150,12 +187,12 @@ class NodeClassification(BaseFlow):
 
             if mask is not None:
                 loss = self.loss_fn(logits[mask], self.labels[mask]).item()
-                metric = self.task.evaluate(logits[mask].argmax(dim=1).to('cpu'), name='f1', mask=mask)
+                metric = self.task.evaluate(logits[mask].argmax(dim=1).to('cpu'), name=self.metric, mask=mask)
                 return metric, loss
             else:
                 masks = {'train': self.train_idx, 'val': self.val_idx, 'test': self.test_idx}
-                metrics = {key: self.task.evaluate(logits[mask].argmax(dim=1).to('cpu'), name='f1', mask=mask) for key, mask in masks.items()}
+                metrics = {key: self.task.evaluate(logits[mask].argmax(dim=1).to('cpu'), name=self.metric, mask=mask) for
+                           key, mask in masks.items()}
                 losses = {key: self.loss_fn(logits[mask], self.labels[mask]).item() for key, mask in masks.items()}
+
                 return metrics, losses
-
-
