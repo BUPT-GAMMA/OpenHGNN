4044528ee4abd5b71bef7b4976e47180f44c2a0e
la <502736806@qq.com>
2021-08-17 15:39:24 +0800

    modify

--- openhgnn/utils/utils.py
+++ openhgnn/utils/utils.py
@@ -56,8 +56,8 @@ class EarlyStopping(object):
             if (score >= self.best_score) and (loss <= self.best_loss):
                 self.save_model(model)
 
-            self.best_loss = loss if loss < self.best_score else self.best_score
-            self.best_score = score if score > self.best_score else self.best_score
+            self.best_loss = np.min((loss,self.best_score))
+            self.best_score = np.max((score, self.best_score))
             self.counter = 0
         return self.early_stop
 
@@ -90,7 +90,7 @@ class EarlyStopping(object):
         else:
             if loss <= self.best_loss:
                 self.save_model(model)
-            self.best_loss = loss if loss < self.best_loss else self.best_loss
+            self.best_loss = np.min((loss, self.best_loss))
             self.counter = 0
         return self.early_stop
 
