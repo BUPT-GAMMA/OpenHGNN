9b00594e9219b96025aabc465edc3c38362bd071
K <1064126026@qq.com>
2021-08-23 10:48:54 +0800

    delete some unnecessary files

--- openhgnn/sampler/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-_log_dir='dblp'
-_log_name='test'
-
-nohup python -u test_MAGNN_sampler.py >logs/$_log_dir/$_log_name 2>&1 &
--- openhgnn/sampler/test_time.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-_log_dir=$1
-_log_name=$2
-kernprof -l -v -u 1 test_MAGNN_sampler.py >logs/$_log_dir/$_log_name 2>&1 &
