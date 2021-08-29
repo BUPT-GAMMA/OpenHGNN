e9d130071756b1d97b021c4ad205b1087b92ff7f
Theheavens <34649403+Theheavens@users.noreply.github.com>
2021-08-29 14:42:10 +0800

    implement mp_GNN

--- openhgnn/models/__init__.py
+++ openhgnn/models/__init__.py
@@ -68,5 +68,6 @@ SUPPORTED_MODELS = {
     'GCN': 'space4hgnn.homo_models.GCN',
     'GAT': 'space4hgnn.homo_models.GAT',
     'homo_GNN': 'space4hgnn.homo_models.homo_GNN',
-    'relation_HGNN': 'space4hgnn.relation_models.rel_HGNN'
+    'relation_HGNN': 'space4hgnn.relation_models.rel_HGNN',
+    'mp_GNN': 'space4hgnn.metapath_models.mp_GNN'
 }
