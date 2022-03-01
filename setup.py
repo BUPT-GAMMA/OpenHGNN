39dc45d471fbe4645f46f4e757edfbad0767910f
dddg617 <996179900@qq.com>
2022-03-01 11:05:10 +0800

    update setup

--- setup.py
+++ setup.py
@@ -4,27 +4,26 @@ from setuptools import setup, find_packages
 
 
 install_requires = ['numpy>=1.16.5', 'pandas>=1.0.0', 'ogb>=1.1.0',
-                    'torch>=1.8.1', 'dgl==0.7a210527', 'optuna']
+                    'torch>=1.8.1', 'optuna']
 setup_requires = []
 tests_require = []
 
 classifiers = [
     'Development Status :: 3 - Alpha',
-    'License :: OSI Approved :: Apache Software License'
-    'Programming Langugage :: Python :: 3.6',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
 
 ]
 
 setup(
     name = "openhgnn",
-    version = "0.1.0",
+    version = "0.2.0",
     author = "BUPT-GAMMA LAB",
     author_email = "tyzhao@bupt.edu.cn",
     maintainer = "Tianyu Zhao",
     license = "Apache-2.0 License",
 
     description = "An open-source toolkit for Heterogeneous Graph Neural Network",
-    long_description = " ",
     
     url = "https://github.com/BUPT-GAMMA/OpenHGNN",
     download_url = "https://github.com/BUPT-GAMMA/OpenHGNN",
@@ -32,10 +31,6 @@ setup(
     python_requires='>=3.6',
 
     packages = find_packages(),
-    
-    package_data = {
-        '': ['*.ini'],
-    },
     
     install_requires = install_requires,
     include_package_data = True,
