210ff98e31c34d33fc076e0b90fa338ff6399650
Jiahang Li <lspongebobjh@gmail.com>
2022-03-02 18:20:22 +0800

    Delete test_config.py
    This file is for testing MAGNN sampler

--- openhgnn/sampler/test_config.py
+++
@@ -1,8 +0,0 @@
-CONFIG={
-    # 'max_epoch': 100, 'patience': 30, 'lr': 0.005, 'weight_decay': 0.001, 'dropout': 0.5, 'hidden_dim': 64,
-    # 'encoder_type': 'RotateE', 'inter_attn_feats': 128, 'num_heads': 8, 'num_layers': 2,
-    # 'dataset': 'imdb4MAGNN', 'gpu':-1, 'batch_size':16
-    'max_epoch': 10, 'patience': 5, 'lr': 0.005, 'weight_decay': 0.001, 'dropout': 0.5, 'hidden_dim': 64,
-    'encoder_type': 'RotateE', 'inter_attn_feats': 128, 'num_heads': 8, 'num_layers': 2, 'out_dim':3,
-    'dataset': 'imdb4MAGNN', 'category':'M', 'batch_size':1, 'device':'cuda:0', 'num_samples':5, 'num_workers':0
-}
