98e44fe63833334e42ffcae9e3294a02442d3f00
Hongyi Zhang <zhanghongyi@bupt.edu.cn>
2022-07-04 10:58:27 +0800

    update dataset

--- openhgnn/dataset/icdm2022_dataset.py
+++ openhgnn/dataset/icdm2022_dataset.py
@@ -1,29 +1,45 @@
 import torch
+import torch as th
 import os
 from dgl.data import DGLBuiltinDataset
-from dgl.data.utils import load_graphs, save_graphs
+from dgl.data.utils import load_graphs, save_graphs, save_info
 import pickle, csv
 import tqdm
-import numpy as np
 
 __all__ = ['ICDM2022Dataset']
+
 
 class ICDM2022Dataset(DGLBuiltinDataset):
     r"""ICDM 2022 Dataset.
 
     Parameters
     ----------
-    scale : str
-        'small' or 'large'.
-
+    session : str
+        'small', 'session1' or 'session2'. The small one is only used for debug.
+    load_features : bool
+        Whether to load features to the graph. Default: True
+    load_labels : bool
+        Whether to load labels to the graph. For session2, there is no labels. Default: True
+    raw_dir : str
+        Specifying the directory that will store the
+        downloaded data or the directory that
+        already stores the input data.
+        Default: ~/.dgl/
+    force_reload : bool
+        Whether to reload the dataset. Default: False
+    verbose : bool
+        Whether to print out progress information. Default: False
+    transform : callable, optional
+        A transform that takes in a :class:`~dgl.DGLGraph` object and returns
+        a transformed version. The :class:`~dgl.DGLGraph` object will be
+        transformed before every access.
     """
 
-    def __init__(self, scale='small', load_feature=True, raw_dir=None,
-                 force_reload=False,
-                 verbose=False,
-                 transform=None):
-        name = 'icdm2022_{}'.format(scale)
-        self.load_feature = load_feature
+    def __init__(self, session='small', load_features=True, load_labels=True, raw_dir=None, force_reload=False,
+                 verbose=False, transform=None):
+        name = 'icdm2022_{}'.format(session)
+        self.load_features = load_features
+        self.load_labels = load_labels
 
         super(ICDM2022Dataset, self).__init__(
             name,
@@ -32,29 +48,15 @@ class ICDM2022Dataset(DGLBuiltinDataset):
             force_reload=force_reload, verbose=verbose, transform=transform)
 
     def process(self):
-        self.load()
 
-    def has_cache(self):
-        graph_path = os.path.join(self.save_path, 'graph.bin')
-        return os.path.exists(graph_path)
-
-    def save(self):
-        pass
-        # graph_path = os.path.join(self.save_path, 'graph.bin')
-        # save_graphs(graph_path, self._g)
-
-    def load(self):
         # load graph
         print('loading dataset...')
         orig_graph_path = os.path.join(self.save_path, '{}.graph.dgl'.format(self.name))
         gs, _ = load_graphs(orig_graph_path)
         g = gs[0]
-        # load node map
-        nodes_path = os.path.join(self.save_path, '{}.nodes.dgl'.format(self.name))
-        with open(nodes_path, 'rb') as f:
-            nodes_info = pickle.load(f)
 
-        if self.load_feature:
+        nodes_info, self._item_map, self._rev_item_map = self._load_map()
+        if self.load_features:
             for ntype, embedding_dict in nodes_info['embeds'].items():
                 dim = embedding_dict[0].shape[0]
                 g.nodes[ntype].data['h'] = torch.empty(g.num_nodes(ntype), dim)
@@ -62,59 +64,79 @@ class ICDM2022Dataset(DGLBuiltinDataset):
                     g.nodes[ntype].data['h'][nid] = torch.from_numpy(embedding)
 
         # load label
-        labels_path = os.path.join(self.save_path, '{}_labels.csv'.format(self.name))
-        labels = torch.tensor([float('nan')] * g.num_nodes(self.category))
-        with open(labels_path, 'r') as f:
+        num_nodes = g.num_nodes(self.category)
+        if self.load_labels:
+            labels_path = os.path.join(self.save_path, '{}_labels.csv'.format(self.name))
+            labels = th.tensor([float('nan')] * g.num_nodes(self.category))
+            with open(labels_path, 'r') as f:
+                csvreader = csv.reader(f)
+                for row in csvreader:
+                    orig_id = int(row[0])
+                    new_id = self._item_map.get(orig_id)
+                    if new_id is not None:
+                        labels[new_id] = int(row[1])
+            label_mask = ~th.isnan(labels)
+            label_idx = th.nonzero(label_mask, as_tuple=False).squeeze()
+            g.nodes[self.category].data['label'] = labels.type(th.int64)
+            split_ratio = [0.9, 0.1]
+            num_labels = len(label_idx)
+            train_mask = th.zeros(num_nodes).bool()
+            train_mask[label_idx[0: int(split_ratio[0] * num_labels)]] = True
+            val_mask = th.zeros(num_nodes).bool()
+            val_mask[
+                label_idx[int(split_ratio[0] * num_labels): int((split_ratio[0] + split_ratio[1]) * num_labels)]] = True
+            g.nodes[self.category].data['train_mask'] = train_mask
+            g.nodes[self.category].data['val_mask'] = val_mask
+
+        # load test_idx
+        test_idx_path = os.path.join(self.save_path, '{}_test_ids.csv'.format(self.name))
+        test_mask = th.zeros(num_nodes).bool()
+        with open(test_idx_path, 'r') as f:
             csvreader = csv.reader(f)
-            item_maps = nodes_info['maps']['item']
             for row in csvreader:
                 orig_id = int(row[0])
-                new_id = item_maps.get(orig_id)
+                new_id = self._item_map.get(orig_id)
                 if new_id is not None:
-                    labels[new_id] = int(row[1])
-
-        label_mask = ~torch.isnan(labels)
-        label_idx = torch.nonzero(label_mask, as_tuple=False).squeeze()
-        g.nodes[self.category].data['label'] = labels.type(torch.int64)
-        split_ratio = [0.9, 0.1]
-        num_labels = len(label_idx)
-        num_nodes = g.num_nodes(self.category)
-        train_mask = torch.zeros(num_nodes).bool()
-        train_mask[label_idx[0: int(split_ratio[0] * num_labels)]] = True
-        val_mask = torch.zeros(num_nodes).bool()
-        val_mask[
-            label_idx[int(split_ratio[0] * num_labels): int((split_ratio[0] + split_ratio[1]) * num_labels)]] = True
-        
-        # test_mask = torch.zeros(num_nodes).bool()
-        # test_mask[label_idx[int((split_ratio[0] + split_ratio[1]) * num_labels):]] = True
-        g.nodes[self.category].data['train_mask'] = train_mask
-        g.nodes[self.category].data['val_mask'] = val_mask
-        # g.nodes[self.category].data['test_mask'] = test_mask
-        # if self.init_emb:
-        #     for ntype in g.ntypes:
-        #         if ntype == 'item':
-        #             g.nodes[ntype].data['h'] = torch.rand(g.num_nodes(ntype), self.item_embedding_dim, dtype=torch.float32)
-        #         else:
-        #             g.nodes[ntype].data['h'] = torch.rand(g.num_nodes(ntype), self.non_item_embedding_dim,
-        #                                                dtype=torch.float32)
-        test_idx_path = '/home/icdm/icdm_graph_competition/OpenHGNN/openhgnn/dataset/test_ids.txt'
-        test_mask = torch.zeros(num_nodes).bool()
-        with open(test_idx_path, 'r') as f:
-            txtreader = np.loadtxt(f, dtype = np.int32)
-            item_maps = nodes_info['maps']['item']
-
-            for row in txtreader:
-                orig_id = int(row)   
-                new_id = item_maps.get(orig_id)
-                if new_id is not None:
-                    # test_mask[label_idx[int((split_ratio[0] + split_ratio[1]) * num_labels):]] = True
-                    # print(new_id)
                     test_mask[new_id] = True
-        
         g.nodes[self.category].data['test_mask'] = test_mask
         self._g = g
 
         print('finish loading dataset')
+
+    def has_cache(self):
+        graph_path = os.path.join(self.save_path, 'graph.bin')
+        return os.path.exists(graph_path)
+
+    def save(self):
+        graph_path = os.path.join(self.save_path, 'graph.bin')
+        save_graphs(graph_path, self._g)
+
+    def load(self):
+        graph_path = os.path.join(self.save_path, 'graph.bin')
+        gs, _ = load_graphs(graph_path)
+        self._g = gs[0]
+        _, self._item_map, self._rev_item_map = self._load_map()
+
+    def _load_map(self):
+        # load node map
+        nodes_path = os.path.join(self.save_path, '{}.nodes.dgl'.format(self.name))
+        with open(nodes_path, 'rb') as f:
+            nodes_info = pickle.load(f)
+
+        # item map and reversed item map
+        item_map = nodes_info['maps']['item']
+        rev_item_map = {}
+        for k, v in item_map.items():
+            rev_item_map[v] = k
+        return nodes_info, item_map, rev_item_map
+
+    @property
+    def item_map(self):
+        return self._item_map
+
+    @property
+    def rev_item_map(self):
+        return self._rev_item_map
 
     @property
     def category(self):
@@ -134,5 +156,3 @@ class ICDM2022Dataset(DGLBuiltinDataset):
 
     def __len__(self):
         return 1
-
-
