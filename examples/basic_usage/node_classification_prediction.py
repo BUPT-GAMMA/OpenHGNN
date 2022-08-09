5134de04197a44489bfa8be6fc94645627d86794
Hongyi Zhang <zhanghongyi@bupt.edu.cn>
2022-08-09 16:12:15 +0800

    add parser

--- examples/basic_usage/node_classification_prediction.py
+++ examples/basic_usage/node_classification_prediction.py
@@ -1,5 +1,6 @@
 import dgl
 import torch
+import argparse
 
 from openhgnn import Experiment
 from openhgnn.dataset import AsNodeClassificationDataset, ACM4GTNDataset
@@ -8,11 +9,32 @@ from dgl.data import CoraFullDataset, CoraGraphDataset
 from sklearn.metrics import f1_score, accuracy_score
 
 if __name__ == '__main__':
-    # acm
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', default='RGCN', type=str, help='name of models')
+    parser.add_argument('--dataset', '-d', default='acm', type=str, help='acm or cora')
+    parser.add_argument('--gpu', '-g', default='-1', type=int, help='-1 means cpu')
+    parser.add_argument('--mini-batch-flag', action='store_true')
 
-    # ds = ACM4GTNDataset()
-    # ds[0].nodes[ds.target_ntype].data['pred_mask'] = ds[0].nodes[ds.target_ntype].data.pop('test_mask')
-    # new_ds = AsNodeClassificationDataset(ds, target_ntype=ds.target_ntype)
+    args = parser.parse_args()
+    # acm
+    if args.dataset == 'acm':
+        ds = ACM4GTNDataset()
+        ds[0].nodes[ds.target_ntype].data['pred_mask'] = ds[0].nodes[ds.target_ntype].data.pop('test_mask')
+        new_ds = AsNodeClassificationDataset(ds, target_ntype=ds.target_ntype)
+
+    # cora graph
+    elif args.dataset == 'cora':
+        ds = CoraGraphDataset()
+        n = ds[0].num_nodes()
+        g = dgl.heterograph(data_dict={('paper', 'cite', 'paper'): ds[0].edges()})
+        g.ndata['train_mask'] = ds[0].ndata['train_mask']
+        g.ndata['val_mask'] = ds[0].ndata['val_mask']
+        g.ndata['test_mask'] = ds[0].ndata['test_mask']
+        g.ndata['pred_mask'] = ds[0].ndata['test_mask']
+        g.ndata['label'] = ds[0].ndata['label']
+        g.ndata['feat'] = ds[0].ndata['feat']
+        new_ds = AsNodeClassificationDataset(g, name='cora-hetero', target_ntype='paper', force_reload=True)
+        new_ds.num_classes = ds.num_classes
 
     # cora full
 
@@ -27,23 +49,9 @@ if __name__ == '__main__':
     #                                      label_mask_feat_name='label_mask', force_reload=True)
     # new_ds.num_classes = ds.num_classes
 
-    # cora graph
-
-    ds = CoraGraphDataset()
-    n = ds[0].num_nodes()
-    g = dgl.heterograph(data_dict={('paper', 'cite', 'paper'): ds[0].edges()})
-    g.ndata['train_mask'] = ds[0].ndata['train_mask']
-    g.ndata['val_mask'] = ds[0].ndata['val_mask']
-    g.ndata['test_mask'] = ds[0].ndata['test_mask']
-    g.ndata['pred_mask'] = ds[0].ndata['test_mask']
-    g.ndata['label'] = ds[0].ndata['label']
-    g.ndata['feat'] = ds[0].ndata['feat']
-    new_ds = AsNodeClassificationDataset(g, name='cora-hetero', target_ntype='paper', force_reload=True)
-    new_ds.num_classes = ds.num_classes
-
     labels = new_ds[0].nodes[new_ds.target_ntype].data['label']
-    experiment = Experiment(model='RGCN', dataset=new_ds, task='node_classification', mini_batch_flag=False, gpu=-1,
-                            test_flag=False, prediction_flag=True)
+    experiment = Experiment(conf_path='./my_config.ini', model=args.model, dataset=new_ds, task='node_classification',
+                            mini_batch_flag=args.mini_batch_flag, gpu=args.gpu, test_flag=False, prediction_flag=True)
     prediction_res = experiment.run()
     indices, y_predicts = prediction_res
     y_predicts = torch.argmax(y_predicts, dim=1)
