740cf3a1cd18b162196815080e9181d8c0f37122
angbak <51525585+NatsuHB@users.noreply.github.com>
2023-03-15 02:01:51 +0800

    Update train.py

--- examples/applications/link_prediction/train.py
+++ examples/applications/link_prediction/train.py
@@ -5,30 +5,17 @@ from dataset import MyLPDatasetWithPredEdges, target_link, target_link_r
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', default='GIE', type=str, help='name of models')
+    parser.add_argument('--model', '-m', default='RGCN', type=str, help='name of models')
     parser.add_argument('--gpu', '-g', default='-1', type=int, help='-1 means cpu')
     parser.add_argument('--mini-batch-flag', action='store_true')
 
     args = parser.parse_args()
     
-    ds = MyDataset()
-    new_ds = AsLinkPredictionDataset(ds, target_link=target_link, target_link_r=None,
+    ds = MyLPDatasetWithPredEdges()
+    new_ds = AsLinkPredictionDataset(ds, target_link=target_link, target_link_r=target_link_r,
                                      split_ratio=[0.8, 0.1, 0.1], force_reload=True)
-    train_graph, pos_val_graph, pos_test_graph, neg_val_graph, neg_test_graph = new_ds.get_split()
-
-
-    def get_triples(hg):
-        g = dgl.to_homogeneous(hg)
-        src, dst = g.edges()
-        rel = g.edata[dgl.ETYPE]
-        return torch.stack((src, rel, dst)).T
-
-
-    new_ds.train_triplets = get_triples(train_graph)
-    new_ds.valid_triplets = get_triples(pos_val_graph)
-    new_ds.test_triplets = get_triples(pos_test_graph)
     
     experiment = Experiment(conf_path='../my_config.ini', max_epoch=3, model=args.model, dataset=new_ds,
                             task='link_prediction', mini_batch_flag=args.mini_batch_flag, gpu=args.gpu,
-                            test_flag=False, prediction_flag=False, batch_size=100, evaluation_metric='mrr')
+                            test_flag=False, prediction_flag=False, batch_size=100)
     experiment.run()
