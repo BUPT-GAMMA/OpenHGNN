e43e450f3033132a073e8a7c99d4450f976185de
clingingsai <15010713603@qq.com>
2023-04-12 16:54:14 +0800

    add MeiREC model

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from openhgnn.experiment import Experiment
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', default='MeiREC', type=str, help='name of models')
-    parser.add_argument('--task', '-t', default='meirec', type=str, help='name of task')
+    parser.add_argument('--model', '-m', default='RGCN', type=str, help='name of models')
+    parser.add_argument('--task', '-t', default='node_classification', type=str, help='name of task')
     # link_prediction / node_classification
-    parser.add_argument('--dataset', '-d', default='meirec', type=str, help='name of datasets')
-    parser.add_argument('--gpu', '-g', default='0', type=int, help='-1 means cpu')
+    parser.add_argument('--dataset', '-d', default='acm4GTN', type=str, help='name of datasets')
+    parser.add_argument('--gpu', '-g', default='-1', type=int, help='-1 means cpu')
     parser.add_argument('--use_best_config', action='store_true', help='will load utils.best_config')
     parser.add_argument('--load_from_pretrained', action='store_true', help='load model from the checkpoint')
     args = parser.parse_args()
