0a143f6a220814d1af5fa94a9dd21d86d6263da9
loadingyy <596911048@qq.com>
2023-09-19 22:34:24 +0800

    final

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import argparse
 
 
 from openhgnn.experiment import Experiment
-'''
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', '-m', default='RGCN', type=str, help='name of models')
@@ -14,22 +13,6 @@ if __name__ == '__main__':
     # link_prediction / node_classification
     parser.add_argument('--dataset', '-d', default='acm4GTN', type=str, help='name of datasets')
     parser.add_argument('--gpu', '-g', default='-1', type=int, help='-1 means cpu')
-    parser.add_argument('--use_best_config', action='store_true', help='will load utils.best_config')
-    parser.add_argument('--load_from_pretrained', action='store_true', help='load model from the checkpoint')
-    args = parser.parse_args()
-
-    experiment = Experiment(model=args.model, dataset=args.dataset, task=args.task, gpu=args.gpu,
-                            use_best_config=args.use_best_config, load_from_pretrained=args.load_from_pretrained)
-
-    experiment.run()
-'''
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--model', '-m', default='SeHGNN', type=str, help='name of models')
-    parser.add_argument('--task', '-t', default='node_classification', type=str, help='name of task')
-    # link_prediction / node_classification
-    parser.add_argument('--dataset', '-d', default='ogbn-mag', type=str, help='name of datasets')
-    parser.add_argument('--gpu', '-g', default='0', type=int, help='-1 means cpu')
     parser.add_argument('--use_best_config', action='store_true', help='will load utils.best_config')
     parser.add_argument('--load_from_pretrained', action='store_true', help='load model from the checkpoint')
     args = parser.parse_args()
