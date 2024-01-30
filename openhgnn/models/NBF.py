ece01eedd52aff9876826a0e18a7254d66f2a2d7
Zhao Zihao <1007105680@qq.com>
2024-01-30 17:56:18 +0800

    modify 1.30

--- openhgnn/models/NBF.py
+++ openhgnn/models/NBF.py
@@ -742,16 +742,10 @@ class GeneralizedRelationalConv(torch.nn.Module):
         if self.aggregate_func == "pna":
             mean = scatter_mean(input * edge_weight, index, dim=self.node_dim, dim_size=dim_size)
             sq_mean = scatter_mean(input ** 2 * edge_weight, index, dim=self.node_dim, dim_size=dim_size)
-<<<<<<< HEAD
 
             max = scatter(input * edge_weight, index, dim=self.node_dim, dim_size=dim_size, reduce="max")
             min= scatter(input * edge_weight, index, dim=self.node_dim, dim_size=dim_size, reduce="min")
    
-=======
-            max = torch.ops.torch_scatter.scatter_max(input * edge_weight, index, dim=self.node_dim , dim_size=dim_size)[0]
-            min = torch.ops.torch_scatter.scatter_min(input * edge_weight, index,dim=self.node_dim ,dim_size=dim_size )[0]
-
->>>>>>> 8a05389e9c2b21fab7944be716227794a468d09f
             std = (sq_mean - mean ** 2).clamp(min=self.eps).sqrt()#Torch
             features = torch.cat([mean.unsqueeze(-1), max.unsqueeze(-1), min.unsqueeze(-1), std.unsqueeze(-1)], dim=-1)
             features = features.flatten(-2)
@@ -866,61 +860,3 @@ class GeneralizedRelationalConv(torch.nn.Module):
 
 
 
-<<<<<<< HEAD
-=======
-def broadcast(src: torch.Tensor, other: torch.Tensor, dim: int):
-    if dim < 0:
-        dim = other.dim() + dim
-    if src.dim() == 1:
-        for _ in range(0, dim):
-            src = src.unsqueeze(0)
-    for _ in range(src.dim(), other.dim()):
-        src = src.unsqueeze(-1)
-    src = src.expand(other.size())
-    return src
-
-
-# scatter_sum == scatter_add
-def scatter_sum(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
-                out: Optional[torch.Tensor] = None,
-                dim_size: Optional[int] = None) -> torch.Tensor:
-    index = broadcast(index, src, dim)
-    if out is None:
-        size = list(src.size())
-        if dim_size is not None:
-            size[dim] = dim_size
-        elif index.numel() == 0:
-            size[dim] = 0
-        else:
-            size[dim] = int(index.max()) + 1
-        out = torch.zeros(size, dtype=src.dtype, device=src.device)
-        return out.scatter_add_(dim, index, src)
-    else:
-        return out.scatter_add_(dim, index, src)
-
-
-def scatter_mean(src: torch.Tensor, index: torch.Tensor, dim: int = -1,
-                 out: Optional[torch.Tensor] = None,
-                 dim_size: Optional[int] = None) -> torch.Tensor:
-    out = scatter_sum(src, index, dim, out, dim_size)
-    dim_size = out.size(dim)
-
-    index_dim = dim
-    if index_dim < 0:
-        index_dim = index_dim + src.dim()
-    if index.dim() <= index_dim:
-        index_dim = index.dim() - 1
-
-    ones = torch.ones(index.size(), dtype=src.dtype, device=src.device)
-    count = scatter_sum(ones, index, index_dim, None, dim_size)
-    count[count < 1] = 1
-    count = broadcast(count, out, dim)
-    if out.is_floating_point():
-        out.true_divide_(count)
-    else:
-        out.div_(count, rounding_mode='floor')
-    return out
-
-
-
->>>>>>> 8a05389e9c2b21fab7944be716227794a468d09f
